76ea560c37ac3854acd2cadfaba3f29b9bc7490a
yarosshest <yarosshest@gmail.com>
2020-09-10 23:47:14 +0300

    add base system

---
+++ EnterInSystem.py
@@ -0,0 +1,138 @@
+import datetime
+from datetime import datetime
+import sqlalchemy
+from sqlalchemy import create_engine, DateTime, func
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship, backref
+import re
+from sqlalchemy.dialects.sqlite import DATETIME
+
+engine = create_engine('sqlite:///some.db', echo=False)
+Base = declarative_base()
+
+
+class Users(Base):
+    __tablename__ = 'Users'
+    id = Column(Integer, primary_key=True)
+    Login = Column(String)
+    Password = Column(String)
+
+    def __init__(self, Login, Password):
+        self.Login = Login
+        self.Password = Password
+
+
+
+
+class Sessions(Base):
+    __tablename__ = 'Sessions'
+    id = Column(Integer, primary_key=True)
+    start_time = Column(DateTime)
+    finish_time = Column(DateTime)
+    Users_id = Column(Integer, ForeignKey('Users.id'))
+    Users = relationship(
+        Users,
+        backref=backref('Sessions',
+                        uselist=True,
+                        cascade='delete,all'))
+
+    def __repr__(self):
+        return "%s to %s" % (self.start_time, self.finish_time)
+
+
+class DatabaseFuction(object):
+    def __init__(self):
+        self.Session = sessionmaker(bind=engine)
+
+    def Insert(self, Log, Pass):
+        NewUser = Users(Log, Pass)
+        session = self.Session()
+        session.add(NewUser)
+        session.commit()
+        session.close()
+
+    def Drop(self):
+        session = self.Session()
+        session.query.delete()
+        session.close()
+
+    def LogOut(self, Log):
+        session = self.Session()
+        for instance in session.query(Sessions.start_time, Sessions.finish_time, Sessions.id):
+            if (None == instance.finish_time):
+                SesionUser = session.query(Sessions).filter_by(id=instance.id).first()
+                SesionUser.finish_time = datetime.now()
+                session.add(SesionUser)
+                session.commit()
+        session.close()
+
+    def Login(self, Log, Pass):
+        Exist = False
+        data = datetime.now()
+        session = self.Session()
+        for instance in session.query(Users.Login, Users.Password, Users.id):
+            if ((instance.Login == Log) and (instance.Password == Pass)):
+                SesionUser = Sessions(start_time=datetime.now(), finish_time=None,
+                                      Users=session.query(Users).filter_by(id=instance.id).first())
+                session.add(SesionUser)
+                session.commit()
+                Exist = True
+        session.close()
+        return Exist
+
+
+    def Register(self, Log, Pass):
+        session = self.Session()
+        Exist = False
+        for instance in session.query(Users.Login):
+            if (instance.Login == Log):
+                Exist = True
+
+        if Exist:
+            session.close()
+            return False
+        else:
+            NewUser = Users(Log, Pass)
+            session.add(NewUser)
+            session.commit()
+            session.close()
+            return True
+        session.close()
+
+
+def LogOutUser(DBase, login):
+    DBase.LogOut(login)
+    return True
+
+
+def LoginUser(DBase, login, password):
+    result = DBase.Login(login, password)
+    if result:
+        return "Login success"
+    else:
+        return "Wrong login password"
+
+
+def RegisterUser(DBase, login, password):
+    result = DBase.Register(login, password)
+    if result:
+        return "Register success"
+    else:
+        return "User already exists"
+
+
+def FullDB(DBase, login, password):
+    DBase.Insert(login, password)
+
+
+def createBd():
+    Base.metadata.create_all(engine)
+    DBase = DatabaseFuction()
+    return DBase
+
+
+def DropTable():
+    Users.__table__.drop(engine)
+    Sessions.__table__.drop(engine)
--- app.py
+++ app.py
@@ -1,12 +1,99 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, session
+# импортируем библиотеку для работы с файловой системой операционной системы
+import os
+import jinja2
+import EnterInSystem
 
 app = Flask(__name__)
 
+# сохраняем в строковую переменную
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+# создаем окружение для шаблона
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
+app.secret_key = '565009BA357CD1A05165F7E729DE7693'
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+template = 'registr.html'
+params = {'name': 'Josh', 'lastname': 'Scogin'}
+t = jinja_env.get_template(template)
+t.render(params)
+session = {}
+session['Login'] = False
+
+# EnterInSystem.DropTable()
+db = EnterInSystem.createBd()
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if session['Login'] and "email" in session:
+        email = session['email']
+        password = session['password']
+        EnterInSystem.LoginUser(db, email, password)
+        return redirect(url_for('UserLab'))
+
+    if request.method == 'POST':
+        email = request.form['Email']
+        password = request.form['password']
+        replay = EnterInSystem.LoginUser(db, email, password)
+        if replay == "Login success":
+            session['email'] = email
+            session['password'] = password
+            session['Login'] = True
+            return redirect(url_for('UserLab'))
+        if replay == "Wrong login password":
+            return render('login.html', err="Неверный логин пароль")
+    else:
+        if "email" in session:
+            email = session['email']
+            password = session['password']
+            return render('login.html', Email=email, password=password)
+        else:
+            return render('login.html')
+
+
+@app.route("/UserLab", methods=['GET', 'POST'])
+def UserLab():
+    if session['Login']:
+        if request.method == 'POST':
+            if "exit" in request.form:
+                email = session['email']
+                EnterInSystem.LogOutUser(db, email)
+                session['Login'] = False
+                return redirect(url_for('login'))
+
+        return render('UserLab.html')
+    else:
+        return redirect(url_for('login'))
+
+
+
+
+@app.route('/registr', methods=['GET', 'POST'])
+def registr():
+    if request.method == 'POST':
+        email = request.form['Email']
+        password = request.form['password']
+        EnterInSystem.RegisterUser(db, email, password)
+        if EnterInSystem.RegisterUser(db, email, password) == "Register success":
+            session['Register'] = True
+            session['email'] = email
+            session['password'] = password
+            return redirect(url_for('login'))
+        else:
+            return render('registr.html', err="Такой email уже есть")
+    else:
+        return render('registr.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return redirect(url_for('login'))
+
+
+def render(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, )
