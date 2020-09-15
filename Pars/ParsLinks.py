7280533b6d4d7ad661f6c2dab0c49863d4accb0a
Алексей <nikita_kotlyarov2002@mail.ru>
2020-09-16 00:49:56 +0300

    vv

--- Pars/ParsLinks.py
+++ Pars/ParsLinks.py
@@ -1,5 +1,4 @@
 from idlelib import browser
-
 from selenium import webdriver
 from selenium.webdriver import chrome
 from selenium.webdriver.android.webdriver import WebDriver
@@ -10,18 +9,32 @@ import urllib
 import time
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.webdriver import WebDriver
-
+import bs4
+import re
+from bs4 import BeautifulSoup
 import EnterInSystem
+import requests
 
 driver: WebDriver = webdriver.Chrome('D:\\chromedriver_win32\\chromedriver.exe')
 
-driver.get("https://www.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=flat&p=2&region=1&type=4")
-
-element_to_hover_over = driver.find_element_by_xpath('//*[@id="frontend-serp"]/div/div[4]/div[1]/div/div[1]')
-hover = ActionChains(driver).move_to_element(element_to_hover_over)
-hover.perform()
-
-href = driver.find_element_by_xpath('//*[@id="frontend-serp"]/div/div[4]/div[1]/div/div[1]/div/div[2]/div[1]/div[1]/div[1]/div/a').get_attribute('href')
-print(href)
+driver.get("https://www.cian.ru/snyat-kvartiru/")
+heig = 500
+heig1 = 0
+driver.execute_script("window.scrollTo(" + str(heig1) + ", " + str(heig) +")")
+for i in range(28):
+    #if i==3:
+    #    i=4
+    #element_to_hover_over = driver.find_element_by_xpath('//*[@id="frontend-serp"]/div/div[6]/div['+str(i+1)+']/div/div[1]')
+    element_to_hover_over = driver.find_element_by_xpath('//*[@id="frontend-serp"]/div/div[6]/article['+str(i+1)+']')
+    hover = ActionChains(driver).move_to_element(element_to_hover_over) #//*[@id="frontend-serp"]/div/div[6]/div[1]/div/div[1] //*[@id="frontend-serp"]/div/div[6]/article[1]/div
+    hover.perform()
+    heig+=500
+    heig1+=500
+    time.sleep(1)
+    #href = driver.find_element_by_xpath('//*[@id="frontend-serp"]/div/div[6]/div[1]/div/div[1]/div/div[2]/div[1]/div[1]/div[1]/div/a').get_attribute('href')
+    href = driver.find_element_by_xpath('//*[@id="frontend-serp"]/div/div[6]/article['+str(i+1)+']/div/div[2]/div[1]/div/a').get_attribute('href')
+    print(href)
+    driver.execute_script("window.scrollTo(" + str(heig1) + ", " + str(heig) +")")
+    print()
 #driver.get(href)
 
