5ff7dda8f04efc8c2a403eb6c444f74582b0c522
Алексей <nikita_kotlyarov2002@mail.ru>
2020-09-20 03:56:30 +0300

    I didn't sleep at this night(((

--- Pars/ParsLinks.py
+++ Pars/ParsLinks.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver import chrome
 from selenium.webdriver.android.webdriver import WebDriver
 from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from selenium import webdriver
 import urllib
 import time
@@ -32,10 +32,19 @@ mini = 27000
 maxi = 27000
 
 while maxi<7000000:
-    driver.find_element_by_id('max-0').clear()
-    driver.find_element_by_name('max').send_keys(maxi)
+    element_to_hover_over = driver.find_element_by_name('min')
+    hover = ActionChains(driver).move_to_element(element_to_hover_over)
+    hover.perform()
+    driver.find_element_by_id("min-0").click()
     driver.find_element_by_id('min-0').clear()
     driver.find_element_by_name('min').send_keys(mini)
+
+    element_to_hover_over = driver.find_element_by_name('max')
+    hover = ActionChains(driver).move_to_element(element_to_hover_over)
+    hover.perform()
+    driver.find_element_by_id("max-0").click()
+    driver.find_element_by_id('max-0').clear()
+    driver.find_element_by_name('max').send_keys(maxi)
     time.sleep(2)
 
     element_to_hover_over = driver.find_element_by_xpath('//button[contains(text(),"Показать")]')
@@ -56,49 +65,42 @@ while maxi<7000000:
 
     flag = True
     while flag:
-            topcol = len(driver.find_elements_by_xpath('//*[@data-name="TopOfferCard"]'))
-            for i in range(topcol-1):
-                # element_to_hover_over = driver.find_elements_by_xpath('//*[@data-name="TopOfferCard"]')[i]
-                # hover = ActionChains(driver).move_to_element(element_to_hover_over)
-                # hover.perform()
-
+        topcol = len(driver.find_elements_by_xpath('//*[contains(@data-name,"OfferCard")]'))
+        for i in range(topcol-1):
+            element_to_hover_over = driver.find_elements_by_xpath('//*[contains(@data-name,"OfferCard")]')[i]
+            hover = ActionChains(driver).move_to_element(element_to_hover_over)
+            hover.perform()
+            try:
+                driver.find_elements_by_xpath('//*[@class="c6e8ba5398--header--1fV2A"]')[i]
+            except StaleElementReferenceException:
+                pass
+            else:
                 href = driver.find_elements_by_xpath('//*[@class="c6e8ba5398--header--1fV2A"]')[i].get_attribute('href')
                 db.addLink(href)
                 print(href)
                 print()
 
-            # offercol = len(driver.find_elements_by_xpath('//*[@data-name="OfferCard"]'))
-            # for i in range(offercol-1):
-            #     element_to_hover_over = driver.find_elements_by_xpath('//*[@data-name="OfferCard"]')[i]
-            #     hover = ActionChains(driver).move_to_element(element_to_hover_over)
-            #     hover.perform()
-            #
-            #     time.sleep(0.05)
-            #     href = driver.find_elements_by_xpath('//*[@class="c6e8ba5398--header--1fV2A"]')[i+topcol].get_attribute('href')
-            #     db.addLink(href)
-            #     print(href)
-            #     print()
+        cardcol = len(driver.find_elements_by_xpath('//*[@data-name="CardComponent"]'))
+        for i in range(cardcol-1):
+            element_to_hover_over = driver.find_elements_by_xpath('//*[@data-name="CardComponent"]')[i]
+            hover = ActionChains(driver).move_to_element(element_to_hover_over)
+            hover.perform()
 
-            cardcol = len(driver.find_elements_by_xpath('//*[@data-name="CardComponent"]'))
-            for i in range(cardcol-1):
-                # element_to_hover_over = driver.find_elements_by_xpath('//*[@data-name="CardComponent"]')[i]
-                # hover = ActionChains(driver).move_to_element(element_to_hover_over)
-                # hover.perform()
-
-                href = driver.find_elements_by_xpath('//*[@class="_93444fe79c--link--39cNw"]')[i].get_attribute('href')
-                db.addLink(href)
-                print(href)
-                print()
-
-            try:
-                driver.find_element_by_link_text(str(j))
-            except NoSuchElementException:
-                flag = False
-            else:
-                element_to_hover_over = driver.find_element_by_link_text(str(j))
-                hover = ActionChains(driver).move_to_element(element_to_hover_over)
-                hover.perform()
-                time.sleep(0.01)
-                driver.find_element_by_link_text(str(j)).click()
+            href = driver.find_elements_by_xpath('//*[@class="_93444fe79c--link--39cNw"]')[i].get_attribute('href')
+            db.addLink(href)
+            print(href)
+            print()
+#        < ul class ="_93444fe79c--list--HEGFW" >
+        print(len(driver.find_elements_by_xpath('//*[@class ="_93444fe79c--list-itemLink--3o7_6"]')))
+        if len(driver.find_elements_by_xpath('//*[@class ="_93444fe79c--list-itemLink--3o7_6"]'))+1<j:
+            flag = False
+        else:
+            element_to_hover_over = driver.find_element_by_xpath('//*[@id="frontend-serp"]/div/div[6]/div/ul')
+            hover = ActionChains(driver).move_to_element(element_to_hover_over)
+            hover.perform()
+            driver.find_element_by_xpath('//*[@id="frontend-serp"]/div/div[6]/div/ul/li['+str(j)+']/a').click()
+            #                             //*[@id="frontend-serp"]/div/div[6]/div/ul/li[11]/a
+            if j < 11:
                 j += 1
-                time.sleep(2)
+            time.sleep(2)
+    driver.execute_script("window.scrollTo(0, 0)")
