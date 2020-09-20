1ddbd5cc56b02ca6b886af9782806ea60d86994c
Алексей <nikita_kotlyarov2002@mail.ru>
2020-09-20 17:06:34 +0300

    The God of Pars

--- Pars/ParsLinks.py
+++ Pars/ParsLinks.py
@@ -25,27 +25,25 @@ db = EnterInSystem.createBd()
 
 driver: WebDriver = webdriver.Chrome('D:\\chromedriver_win32\\chromedriver.exe')
 
-driver.get("https://www.cian.ru/snyat-kvartiru/")
-driver.set_window_size(1920,1080)
-
-mini = 27000
+mini = 0
 maxi = 27000
 
 while maxi<7000000:
+    driver.get("https://www.cian.ru/snyat-kvartiru/")
+    driver.set_window_size(1920, 1080)
+
     element_to_hover_over = driver.find_element_by_name('min')
     hover = ActionChains(driver).move_to_element(element_to_hover_over)
     hover.perform()
     driver.find_element_by_id("min-0").click()
-    driver.find_element_by_id('min-0').clear()
     driver.find_element_by_name('min').send_keys(mini)
 
     element_to_hover_over = driver.find_element_by_name('max')
     hover = ActionChains(driver).move_to_element(element_to_hover_over)
     hover.perform()
     driver.find_element_by_id("max-0").click()
-    driver.find_element_by_id('max-0').clear()
     driver.find_element_by_name('max').send_keys(maxi)
-    time.sleep(2)
+    time.sleep(1)
 
     element_to_hover_over = driver.find_element_by_xpath('//button[contains(text(),"Показать")]')
     hover = ActionChains(driver).move_to_element(element_to_hover_over)
@@ -65,11 +63,12 @@ while maxi<7000000:
 
     flag = True
     while flag:
+        time.sleep(1)
         topcol = len(driver.find_elements_by_xpath('//*[contains(@data-name,"OfferCard")]'))
+        print(topcol)
         for i in range(topcol-1):
             element_to_hover_over = driver.find_elements_by_xpath('//*[contains(@data-name,"OfferCard")]')[i]
             hover = ActionChains(driver).move_to_element(element_to_hover_over)
-            hover.perform()
             try:
                 driver.find_elements_by_xpath('//*[@class="c6e8ba5398--header--1fV2A"]')[i]
             except StaleElementReferenceException:
@@ -79,28 +78,28 @@ while maxi<7000000:
                 db.addLink(href)
                 print(href)
                 print()
+                time.sleep(0.02)
 
         cardcol = len(driver.find_elements_by_xpath('//*[@data-name="CardComponent"]'))
         for i in range(cardcol-1):
             element_to_hover_over = driver.find_elements_by_xpath('//*[@data-name="CardComponent"]')[i]
             hover = ActionChains(driver).move_to_element(element_to_hover_over)
-            hover.perform()
 
             href = driver.find_elements_by_xpath('//*[@class="_93444fe79c--link--39cNw"]')[i].get_attribute('href')
             db.addLink(href)
             print(href)
             print()
-#        < ul class ="_93444fe79c--list--HEGFW" >
+        time.sleep(0.02)
+
+        time.sleep(1)
         print(len(driver.find_elements_by_xpath('//*[@class ="_93444fe79c--list-itemLink--3o7_6"]')))
         if len(driver.find_elements_by_xpath('//*[@class ="_93444fe79c--list-itemLink--3o7_6"]'))+1<j:
             flag = False
         else:
-            element_to_hover_over = driver.find_element_by_xpath('//*[@id="frontend-serp"]/div/div[6]/div/ul')
+            element_to_hover_over = driver.find_element_by_xpath('//*[@class="_93444fe79c--list--HEGFW"]/li['+str(j)+']')
             hover = ActionChains(driver).move_to_element(element_to_hover_over)
-            hover.perform()
-            driver.find_element_by_xpath('//*[@id="frontend-serp"]/div/div[6]/div/ul/li['+str(j)+']/a').click()
-            #                             //*[@id="frontend-serp"]/div/div[6]/div/ul/li[11]/a
+            driver.find_element_by_xpath('//*[@class="_93444fe79c--list--HEGFW"]/li['+str(j)+']/a').click()
+            #                             //*[@id="frontend-serp"]/div/div[7]/div/ul/li[2]/a<ul class="_93444fe79c--list--HEGFW">
             if j < 11:
                 j += 1
-            time.sleep(2)
-    driver.execute_script("window.scrollTo(0, 0)")
+            time.sleep(1)
